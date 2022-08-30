6a034b41673cf2dfa947b5d882e87513d9ea678d
Salvador318Alvarez <Salvador318Alvarez@gmail.com>
2022-08-30 16:34:25 -0500

    moved data to a json file for json server

--- src/Home.js
+++ src/Home.js
@@ -3,23 +3,23 @@ import List from './List';
 
 const Home = () => {
 
-    const [songs, setSongs] = useState([
-        {title:"Jesus, I My Cross Have Taken", songBook:"Soul-Stirring Song & Hymns", id: 1},
-        {title:"Glory to His Name", songBook:"Soul-Stirring Song & Hymns", id: 2},
-        {title:"Jesus Paid It All", songBook:"Soul-Stirring Song & Hymns",  id: 3},
-        {title:"The Way of the Cross", songBook:"Soul-Stirring Song & Hymns", id: 4},
-        {title: "When I Survey", songBook:"Soul-Stirring Song & Hymns", id:5},
-        {title: "Beneath the Cross of Jesus", songBook:"Soul-Stirring Song & Hymns", id:6},
-        {title: "I Gave My Life for Thee", songBook:"Soul-Stirring Song & Hymns", id: 7},
-        {title: "I am Coming", songBook:"Soul-Stirring Song & Hymns", id:8},
-        {title: "Room at the Cross", songBook:"Soul-Stirring Song & Hymns", id:9},
-        {title: "Near the Cross", songBook:"Soul-Stirring Song & Hymns", id:10}
-      ]);
+    const [songs, setSongs] = useState(null);
 
     const handleDelete = (id) => {
         const newSongs = songs.filter(song => song.id !== id);
         setSongs(newSongs);
     }
+
+    useEffect(() => {
+        fetch('http://localhost:8000/songs')
+        .then(res => {
+            return res.json();
+        })
+        .then((data) =>{
+            setSongs(data);
+        });
+        console.log("useEffect ran");
+    }, []);
 
     const reloadPage = () => {
         window.location.reload();
@@ -28,7 +28,7 @@ const Home = () => {
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <List songs={songs} handleDelete={handleDelete}/>
+            {songs && <List songs={songs} handleDelete={handleDelete}/>}
             <button onClick={reloadPage}>Refresh Page</button>
         </div>
     );
